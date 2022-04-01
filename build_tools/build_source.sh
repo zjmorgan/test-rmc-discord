a618773c15aff0323df5759d132601feff2c98f9
Zachary Morgan <zjmorgan@mtu.edu>
2022-04-01 18:31:06 -0400

    Create build_source.sh

---
+++ build_tools/build_source.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+set -x
+
+cd ../../
+
+python -m venv build_env
+source build_env/bin/activate
+
+python -m pip install numpy scipy cython
+python -m pip install twine
+
+cd rmc-discord/rmc-discord
+python setup.py sdist
+
+# Check whether the source distribution will render correctly
+twine check dist/*.tar.gz
