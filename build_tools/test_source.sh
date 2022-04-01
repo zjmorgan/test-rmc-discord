9c8af4053b4498ff497e482d7a31d384bfb8f440
Zachary Morgan <zjmorgan@mtu.edu>
2022-04-01 18:33:11 -0400

    Create test_source.sh

---
+++ build_tools/test_source.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+set -e
+set -x
+
+cd ../../
+
+python -m venv test_env
+source test_env/bin/activate
+
+python -m pip install rmc-discord/rmc-discord/dist/*.tar.gz
+python -m pip install pytest
+
+# Run the tests on the installed source distribution
+mkdir tmp_for_test
+cd tmp_for_test
+
+python -m pytest --pyargs disorder
