2e26fe139c0635de4e3827897850eaaaba11531d
Zachary Morgan <zjmorgan@mtu.edu>
2022-04-01 21:02:56 -0400

    Update test_source.sh

--- build_tools/test_source.sh
+++ build_tools/test_source.sh
@@ -8,7 +8,7 @@ cd ../../
 python -m venv test_env
 source test_env/bin/activate
 
-python -m pip install rmc-discord/rmc-discord/dist/*.tar.gz
+python -m pip install test-rmc-discord/test-rmc-discord/dist/*.tar.gz
 python -m pip install pytest
 
 # Run the tests on the installed source distribution
