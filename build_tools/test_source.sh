b8917568b72903554f9f2f324d5d2e0705b4335b
Zachary Morgan <zjmorgan@mtu.edu>
2022-04-02 07:36:18 -0400

    Update test_source.sh

--- build_tools/test_source.sh
+++ build_tools/test_source.sh
@@ -8,6 +8,9 @@ cd ../../
 python -m venv test_env
 source test_env/bin/activate
 
+sudo apt install libxcb-icccm4 libxcb-image0 libxcb-keysyms1 libxcb-randr0 libxcb-render-util0 libxcb-xinerama0 libxcb-xfixes0
+
+export QT_QPA_PLATFORM=offscreen
 python -m pip install test-rmc-discord/test-rmc-discord/dist/*.tar.gz
 python -m pip install pytest
 
