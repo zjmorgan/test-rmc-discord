8077d12a8a8dcbb45f15b01253124956bf2c4880
Zachary Morgan <zjmorgan@mtu.edu>
2022-04-01 23:17:00 -0400

    Update test_source.sh

--- build_tools/test_source.sh
+++ build_tools/test_source.sh
@@ -11,7 +11,4 @@ source test_env/bin/activate
 python -m pip install test-rmc-discord/test-rmc-discord/dist/*.tar.gz
 python -m pip install pytest
 
-# Run the tests on the installed source distribution
-cd test-rmc-discord/test-rmc-discord
-
-python -m pytest --pyargs disorder
+python -m pytest --pyargs test-rmc-discord/test-rmc-discord/disorder
